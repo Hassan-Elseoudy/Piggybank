3686cd2853d63a3af93fe1777413fec9265f66bf
hassan.elseoudy <hassan.elseoudy@gmail.com>
2021-12-02 22:56:53 +0200

    :zap: Using some filters

--- core/views.py
+++ core/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import filters
 
 from core.models import Currency, Category, Transaction
 from core.serializers import CurrencySerializer, CategorySerializer, WriteTransactionSerializer, \
@@ -22,6 +25,10 @@ class CategoryModelViewSet(ModelViewSet):
 
 class TransactionModelViewSet(ModelViewSet):
     queryset = Transaction.objects.select_related("currency", "category")
+    filter_backends = [filters.SearchFilter, OrderingFilter, DjangoFilterBackend]
+    search_fields = ['description']
+    ordering_fields = ['amount', 'date']
+    filterset_fields = ["currency__code",]
 
     def get_serializer_class(self):
         if self.action in ("list", "retrieve"):
